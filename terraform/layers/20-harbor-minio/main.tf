e6f116f7703e3272f06481615629253406822819
csning1998 <january20070120@gmail.com>
2025-12-25 01:33:19 +0800

    feat(MinIO): Add `aminueza/minio` provider and configure required buckets for MinIO startup.

--- terraform/layers/20-harbor-minio/main.tf
+++ terraform/layers/20-harbor-minio/main.tf
@@ -8,3 +8,12 @@ module "minio_harbor" {
   vault_role_name   = "harbor-minio"
   vault_ca_cert_b64 = filebase64("${path.root}/../10-vault-core/tls/vault-ca.crt")
 }
+
+module "minio_harbor_config" {
+  source     = "../../modules/28-minio-config"
+  depends_on = [module.minio_harbor]
+
+  minio_tenants            = var.harbor_minio_tenants
+  vault_secret_path_prefix = "secret/on-premise-gitlab-deployment/harbor/s3_credentials"
+  minio_server_url         = "https://${var.harbor_minio_compute.ha_config.virtual_ip}:9000"
+}
