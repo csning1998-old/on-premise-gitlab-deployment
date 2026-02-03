5523794af9408d9ce8f7d4fb309ddcc7d70f3b0f
csning1998 <csning1998@gmail.com>
2026-02-04 01:53:15 +0800

    refactor(kubeadm): Set Ingress Controller Service Type to NodePort
    Explicitly specify `nodePorts` as `http: 30080` and `https: 30443` in the Helm Chart to ensure HAProxy backend configurations remain valid.

--- terraform/layers/30-gitlab-kubeadm/data.tf
+++ terraform/layers/30-gitlab-kubeadm/data.tf
@@ -5,3 +5,11 @@ data "terraform_remote_state" "vault_core" {
     path = "../10-vault-core/terraform.tfstate"
   }
 }
+
+data "terraform_remote_state" "cluster_provision" {
+  backend = "local"
+  config = {
+    path = "../30-gitlab-kubeadm/terraform.tfstate"
+  }
+}
+
