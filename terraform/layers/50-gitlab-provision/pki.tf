f4860101ff171c39fde169be4489844dd418d083
csning1998 <csning1998@gmail.com>
2026-02-03 19:45:20 +0800

    feat(gitlab-helm): Complete basic GitLab Helm Chart deployment
    To fix: Resolve certificate issue for Redis VIP `172.16.134.250`, specifically `data.terraform_remote_state.gitlab_redis.outputs.gitlab_redis_virtual_ip`.

---
+++ terraform/layers/50-gitlab-provision/pki.tf
@@ -0,0 +1,28 @@
+
+
+# Ingress Certificate
+resource "vault_pki_secret_backend_cert" "gitlab_ingress_cert" {
+  backend = data.terraform_remote_state.vault_core.outputs.pki_configuration.vault_pki_path
+  name    = data.terraform_remote_state.vault_core.outputs.pki_configuration.ingress_roles.gitlab
+
+  common_name = data.terraform_remote_state.vault_core.outputs.pki_configuration.ingress_domains.gitlab[0]
+  ttl         = "2160h"
+}
+
+# Inject Certificate to Kubernetes
+resource "kubernetes_secret" "gitlab_tls" {
+  metadata {
+    name      = "gitlab-tls"
+    namespace = kubernetes_namespace.gitlab.metadata[0].name
+  }
+
+  type = "kubernetes.io/tls"
+
+  data = {
+    "tls.crt" = join("\n", [
+      vault_pki_secret_backend_cert.gitlab_ingress_cert.certificate,
+      vault_pki_secret_backend_cert.gitlab_ingress_cert.ca_chain
+    ])
+    "tls.key" = vault_pki_secret_backend_cert.gitlab_ingress_cert.private_key
+  }
+}
--- terraform/layers/50-gitlab-provision/resources-redis.tf
+++ terraform/layers/50-gitlab-provision/resources-redis.tf
@@ -5,10 +5,11 @@ resource "vault_generic_secret" "gitlab_redis_keys" {
 
   data_json = jsonencode({
     # Use variables to drive IP & Port
-    host     = data.terraform_remote_state.gitlab_redis.outputs.gitlab_redis_virtual_ip
+    # host     = data.terraform_remote_state.gitlab_redis.outputs.gitlab_redis_virtual_ip
+    host     = "172.16.141.200" # TODO: Use variables to drive IP & Port
     port     = data.terraform_remote_state.gitlab_redis.outputs.gitlab_redis_haproxy_stats_port
     password = data.vault_generic_secret.db_vars.data["redis_requirepass"]
-    scheme   = "redis"
+    scheme   = "rediss"
   })
 }
 
