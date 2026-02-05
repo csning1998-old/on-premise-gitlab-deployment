960776581e4cdcf8956815c68e87a188ef4e3375
csning1998 <csning1998@gmail.com>
2026-02-06 03:28:28 +0800

    refactor(sentinel-pki): Implement AppRole authentication for GitLab / Harbor Sentinel identity.

--- terraform/layers/20-gitlab-redis/main.tf
+++ terraform/layers/20-gitlab-redis/main.tf
@@ -1,12 +1,22 @@
 
+# Call the Identity Module to generate AppRole & Secret ID
+module "redis_identity" {
+  source = "../../modules/configuration/vault-workload-identity"
+
+  name            = var.gitlab_redis_compute.cluster_identity.service_name
+  vault_role_name = local.vault_role_name
+}
+
 module "redis_gitlab" {
   source = "../../modules/service-ha/sentinel-cluster"
 
-  topology_config   = var.gitlab_redis_compute
-  infra_config      = var.gitlab_redis_infra
-  service_domain    = local.service_domain
-  vault_role_name   = local.vault_role_name
-  vault_ca_cert_b64 = filebase64("${path.root}/../10-vault-core/tls/vault-ca.crt")
+  enable_tls      = true
+  topology_config = var.gitlab_redis_compute
+  infra_config    = var.gitlab_redis_infra
+  service_domain  = local.service_domain
 
-  enable_tls = true
+  vault_approle_role_id   = module.redis_identity.role_id
+  vault_approle_secret_id = module.redis_identity.secret_id
+  vault_role_name         = local.vault_role_name
+  vault_ca_cert_b64       = filebase64("${path.root}/../10-vault-core/tls/vault-ca.crt")
 }
