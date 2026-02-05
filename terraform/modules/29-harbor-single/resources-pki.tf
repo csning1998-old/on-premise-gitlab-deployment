a9bfccdbe087149b578a28fe07ea12c8ba35eac7
csning1998 <csning1998@gmail.com>
2026-02-05 21:29:39 +0800

    fix(dev-harbor): Removed dead code to avoid confusion.

--- terraform/modules/29-harbor-single/resources-pki.tf
+++ terraform/modules/29-harbor-single/resources-pki.tf
@@ -28,22 +28,6 @@ resource "vault_approle_auth_backend_role" "dev_harbor" {
   token_max_ttl = 60 * 60 * 24
 }
 
-resource "vault_pki_secret_backend_role" "dev_harbor_client" {
-  backend            = var.vault_pki_mount_path
-  name               = "dev-harbor-client-role"
-  allowed_domains    = ["dev-harbor", "dev-harbor.iac.local"]
-  allow_subdomains   = true
-  allow_ip_sans      = true
-  allow_any_name     = false
-  allow_bare_domains = true
-  key_type           = "rsa"
-  key_bits           = 2048
-  key_usage          = ["DigitalSignature", "KeyAgreement", "KeyEncipherment"]
-  ttl                = 60 * 60 * 24
-  client_flag        = true
-  server_flag        = false
-}
-
 # Generate Secret ID for login credentials
 resource "vault_approle_auth_backend_role_secret_id" "dev_harbor" {
   backend   = vault_approle_auth_backend_role.dev_harbor.backend
