bd2eb0fd09c6d7b1046f832865a0776b16f5be64
csning1998 <csning1998@gmail.com>
2026-02-07 00:55:15 +0800

    refactor(sentinel): Inject all secrets from the `20-gitlab-redis` layer into the sentinel module; verified failover is functional.

--- terraform/layers/20-gitlab-redis/locals.tf
+++ terraform/layers/20-gitlab-redis/locals.tf
@@ -1,12 +1,23 @@
 
-# For Redis Service Configuration
 locals {
-  platform_id    = var.gitlab_redis_compute.cluster_identity.service_name
-  service_domain = local.domain_list[0] # redis.gitlab.iac.local
-}
+  # Service Identity
+  svc_name  = var.gitlab_redis_compute.cluster_identity.service_name
+  comp_name = var.gitlab_redis_compute.cluster_identity.component
 
-# For PKI Configuration
-locals {
-  vault_role_name = data.terraform_remote_state.vault_core.outputs.pki_configuration.roles.redis[local.platform_id]
-  domain_list     = data.terraform_remote_state.vault_core.outputs.pki_configuration.domains.redis[local.platform_id]
+  # Naming Convention
+  nat_net_name      = "iac-${local.svc_name}-${local.comp_name}-nat"
+  hostonly_net_name = "iac-${local.svc_name}-${local.comp_name}-hostonly"
+  storage_pool_name = "iac-${local.svc_name}-${local.comp_name}"
+
+  # Bridges
+  svc_abbr             = substr(local.svc_name, 0, 3)
+  comp_abbr            = substr(local.comp_name, 0, 3)
+  nat_bridge_name      = "${local.svc_abbr}-${local.comp_abbr}-natbr"
+  hostonly_bridge_name = "${local.svc_abbr}-${local.comp_abbr}-hostbr"
+
+  # PKI & Domain Logic
+  platform_id     = local.svc_name
+  service_domain  = local.domain_list[0]
+  vault_role_name = data.terraform_remote_state.vault_core.outputs.pki_configuration.database_roles["${local.platform_id}-redis"].name
+  domain_list     = data.terraform_remote_state.vault_core.outputs.pki_configuration.database_roles["${local.platform_id}-redis"].allowed_domains
 }
