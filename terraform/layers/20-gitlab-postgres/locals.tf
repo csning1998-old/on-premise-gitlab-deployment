5b974d90f03344d30372cdb1334c73532f4d5bc1
csning1998 <csning1998@gmail.com>
2026-02-06 18:00:43 +0800

    refactor(patroni): Inject all secrets from the `20-gitlab-postgres` layer into the module; verified failover is functional.

--- terraform/layers/20-gitlab-postgres/locals.tf
+++ terraform/layers/20-gitlab-postgres/locals.tf
@@ -1,12 +1,23 @@
 
-# For Postgres Service Configuration
 locals {
-  platform_id    = var.gitlab_postgres_compute.cluster_identity.service_name
-  service_domain = local.domain_list[0] # pg.gitlab.iac.local
-}
+  # Service Identity
+  svc_name  = var.harbor_postgres_compute.cluster_identity.service_name
+  comp_name = var.harbor_postgres_compute.cluster_identity.component
 
-# For PKI Configuration
-locals {
-  vault_role_name = data.terraform_remote_state.vault_core.outputs.pki_configuration.roles.postgres[local.platform_id]
-  domain_list     = data.terraform_remote_state.vault_core.outputs.pki_configuration.domains.postgres[local.platform_id]
+  # Naming Convention
+  nat_net_name      = "iac-${local.svc_name}-${local.comp_name}-nat"
+  hostonly_net_name = "iac-${local.svc_name}-${local.comp_name}-hostonly"
+  storage_pool_name = "iac-${local.svc_name}-${local.comp_name}"
+
+  # Bridges
+  svc_abbr             = substr(local.svc_name, 0, 3)
+  comp_abbr            = substr(local.comp_name, 0, 3)
+  nat_bridge_name      = "${local.svc_abbr}-${local.comp_abbr}-natbr"
+  hostonly_bridge_name = "${local.svc_abbr}-${local.comp_abbr}-hostbr"
+
+  # PKI & Domain Logic
+  platform_id     = local.svc_name
+  service_domain  = local.domain_list[0]
+  vault_role_name = data.terraform_remote_state.vault_core.outputs.pki_configuration.database_roles["${local.platform_id}-postgres"].name
+  domain_list     = data.terraform_remote_state.vault_core.outputs.pki_configuration.database_roles["${local.platform_id}-postgres"].allowed_domains
 }
--- terraform/layers/20-gitlab-postgres/main.tf
+++ terraform/layers/20-gitlab-postgres/main.tf
@@ -1,21 +1,49 @@
 
-# Call the Identity Module to generate AppRole & Secret ID
 module "postgres_identity" {
   source = "../../modules/configuration/vault-workload-identity"
 
   name            = var.gitlab_postgres_compute.cluster_identity.service_name
   vault_role_name = local.vault_role_name
+
+  pki_mount_path     = data.terraform_remote_state.vault_core.outputs.pki_configuration.path
+  approle_mount_path = data.terraform_remote_state.vault_core.outputs.auth_backend_paths["approle"]
 }
 
 module "postgres_gitlab" {
   source = "../../modules/service-ha/patroni-cluster"
 
+  # Topology
   topology_config = var.gitlab_postgres_compute
   infra_config    = var.gitlab_postgres_infra
   service_domain  = local.service_domain
 
-  vault_approle_role_id   = module.postgres_identity.role_id
-  vault_approle_secret_id = module.postgres_identity.secret_id
-  vault_role_name         = local.vault_role_name
-  vault_ca_cert_b64       = filebase64("${path.root}/../10-vault-core/tls/vault-ca.crt")
+  # Network Identity
+  network_identity = {
+    nat_net_name         = local.nat_net_name
+    nat_bridge_name      = local.nat_bridge_name
+    hostonly_net_name    = local.hostonly_net_name
+    hostonly_bridge_name = local.hostonly_bridge_name
+    storage_pool_name    = local.storage_pool_name
+  }
+
+  # Credentials Injection
+  vm_credentials = {
+    username             = data.vault_generic_secret.iac_vars.data["vm_username"]
+    password             = data.vault_generic_secret.iac_vars.data["vm_password"]
+    ssh_public_key_path  = data.vault_generic_secret.iac_vars.data["ssh_public_key_path"]
+    ssh_private_key_path = data.vault_generic_secret.iac_vars.data["ssh_private_key_path"]
+  }
+
+  db_credentials = {
+    superuser_password   = data.vault_generic_secret.db_vars.data["pg_superuser_password"]
+    replication_password = data.vault_generic_secret.db_vars.data["pg_replication_password"]
+    vrrp_secret          = data.vault_generic_secret.db_vars.data["pg_vrrp_secret"]
+  }
+
+  vault_agent_config = {
+    role_id     = module.postgres_identity.approle_role_id
+    secret_id   = module.postgres_identity.approle_secret_id
+    ca_cert_b64 = filebase64("${path.root}/../10-vault-core/tls/vault-ca.crt")
+    role_name   = local.vault_role_name
+  }
 }
