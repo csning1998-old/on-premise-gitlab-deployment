a718ad12d23bfc3b686467db8ff733f64de61f0b
csning1998 <csning1998@gmail.com>
2026-03-10 20:33:51 +0800

    fix(terraform): replace `skip_tls_verify = true` with a bootstrapper-specific Vault public key on the host.

--- terraform/layers/30-bootstrapper-harbor-platform/variables.tf
+++ terraform/layers/30-bootstrapper-harbor-platform/variables.tf
@@ -1,6 +1,6 @@
 
 variable "vault_dev_addr" {
-  description = "The address of the Vault server"
+  description = "The address of the Bootstrapper Vault server"
   type        = string
   default     = "https://127.0.0.1:8200"
 }
