d8c911bf3945bc5de28da76e377870f5e3ce69af
csning1998 <csning1998@gmail.com>
2026-03-11 12:31:26 +0800

    feat(libvirt-disk): enable dynamic mounting of independent disks with variable counts and sizes per node. Vault as Example now.

--- terraform/modules/cluster-provision/hypervisor-kvm/main.tf
+++ terraform/modules/cluster-provision/hypervisor-kvm/main.tf
@@ -106,13 +106,23 @@ resource "libvirt_volume" "os_disk" {
   name     = "${each.key}-os.qcow2"
   pool     = libvirt_pool.storage_pool.name
   format   = "qcow2"
-  capacity = 42949672960 # 40 GiB (Must match or exceed the base image virtual size; Variable control needed.)
+  capacity = each.value.os_disk_capacity_gib * 1024 * 1024 * 1024
 
   # Use Copy-on-Write
   backing_store = {
     path   = libvirt_volume.base_image[basename(abspath(each.value.base_image_path))].path
     format = "qcow2"
   }
+}
+
+resource "libvirt_volume" "data_disk" {
+  depends_on = [libvirt_pool.storage_pool]
+  for_each   = local.data_disks_flat
+
+  name     = "${each.key}.qcow2"
+  pool     = libvirt_pool.storage_pool.name
+  format   = "qcow2"
+  capacity = each.value.capacity_gib * 1024 * 1024 * 1024 # Libvirt requires volume capacity to be declared in Bytes.
 }
 
 resource "libvirt_cloudinit_disk" "cloud_init" {
@@ -168,7 +178,7 @@ resource "libvirt_domain" "nodes" {
   # 1. Basic Configuration (Required)
   name      = each.key
   vcpu      = each.value.vcpu
-  memory    = each.value.ram
+  memory    = each.value.ram_size
   unit      = "MiB"
   autostart = false
   running   = true
@@ -181,9 +191,9 @@ resource "libvirt_domain" "nodes" {
 
   # 3. Hardware Device Configuration (Attributes)
   devices = {
-    disks = [
-      # First Disk: Operating System
-      {
+    disks = concat(
+      # 1. OS Disk (vda)
+      [{
         device = "disk"
         target = {
           dev = "vda"
@@ -193,9 +203,24 @@ resource "libvirt_domain" "nodes" {
           pool   = libvirt_pool.storage_pool.name
           volume = libvirt_volume.os_disk[each.key].name
         }
-      },
-      # Second Disk: Cloud-Init ISO
-      {
+      }],
+
+      # 2. Data Disks (vdb, vdc...)
+      [for idx, disk in each.value.data_disks : {
+        device = "disk"
+        target = {
+          # idx=0 -> vdb, idx=1 -> vdc
+          dev = "vd${substr("bcdefghijklmnopqrstuvwxyz", idx, 1)}"
+          bus = "virtio"
+        }
+        source = {
+          pool   = libvirt_pool.storage_pool.name
+          volume = libvirt_volume.data_disk["${each.key}-${disk.name_suffix}"].name
+        }
+      }],
+
+      # 3. Cloud-Init (sda)
+      [{
         device = "cdrom"
         target = {
           dev = "sda"
@@ -205,8 +230,8 @@ resource "libvirt_domain" "nodes" {
           pool   = libvirt_pool.storage_pool.name
           volume = libvirt_volume.cloud_init_iso[each.key].name
         }
-      }
-    ]
+      }]
+    )
 
     # Network Interfaces search by network tier
     interfaces = [
