24f5bc49aa9e607b0c0ff4a5f79ee2f1111279d3
csning1998 <csning1998@gmail.com>
2026-03-17 00:54:58 +0800

    chore: Remove redundant vault provider in network layer 05,

--- terraform/layers/05-foundation-network/providers.tf
+++ terraform/layers/05-foundation-network/providers.tf
@@ -5,18 +5,9 @@ terraform {
       source  = "dmacvicar/libvirt"
       version = "0.9.0"
     }
-    vault = {
-      source  = "hashicorp/vault"
-      version = "5.5.0"
-    }
   }
 }
 
 provider "libvirt" {
   uri = "qemu:///system"
-}
-
-provider "vault" {
-  address      = var.vault_dev_addr
-  ca_cert_file = abspath("${path.root}/../../../vault/tls/ca.pem")
 }
